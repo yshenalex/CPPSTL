aa0cf20afe5dfc20fa5e6cc7c1b2b8373f1b4238
yshenalex <2016057981@qq.com>
2024-07-29 22:31:57 +0800

    .

--- BSTree/BSTree/BSTree.h
+++ BSTree/BSTree/BSTree.h
@@ -27,25 +27,35 @@ namespace K
 		BSTree()
 			:_root(nullptr)
 		{}
-		
-		void Destroy(Node* root)//析构函数没办法递归，故如此处理
-		{
-			if (!root)
-				return;
 
-			Destroy(root->_left);
-			Destroy(root->_right);
-			delete root;
+
+		BSTree(const BSTree<K>& t)
+		{
+			_root = _Copy(t._root);
 		}
+
+		//BSTree<K>& operator=(const BSTree<K>& t)
+		//{
+		//	_root = _Copy(t._root);
+
+		//	return *this;
+		//}
+
+		BSTree<K>& operator=(BSTree<K> t)
+		{
+			swap(t._root, _root);
+			return *this;
+		}
+
 
 		~BSTree()
 		{
-			Destroy(_root);
+			_Destroy(_root);
 			_root = nullptr;
 		}
 
 
-		bool Find(const K& x)
+		Node* Find(const K& x)
 		{
 			Node* cur = _root;
 
@@ -61,11 +71,11 @@ namespace K
 				}
 				else
 				{
-					return true;
+					return cur;
 				}
 			}
-			
-			return false;
+
+			return nullptr;
 		}
 
 		bool Insert(const K& x)
@@ -205,6 +215,38 @@ namespace K
 			return false;
 		}
 
+
+
+		void InOrder()
+		{
+			_InOrder(_root);
+			cout << endl;
+		}
+
+
+	private:
+		Node* _Copy(Node* root)
+		{
+			if (!root)
+				return nullptr;
+
+			Node* newroot = new Node(root->_key);
+			newroot->_left = _Copy(root->_left);
+			newroot->_right = _Copy(root->_right);
+
+			return newroot;
+		}
+
+		void _Destroy(Node* root)//析构函数没办法递归，故如此处理
+		{
+			if (!root)
+				return;
+
+			_Destroy(root->_left);
+			_Destroy(root->_right);
+			delete root;
+		}
+
 		void _InOrder(Node* root)
 		{
 			if (!root)
@@ -214,16 +256,111 @@ namespace K
 			cout << root->_key << " ";
 			_InOrder(root->_right);
 		}
-		void InOrder()
-		{
-			_InOrder(_root);
-			cout << endl;
-		}
-
 
 	private:
 		Node* _root = nullptr;
 	};
 
 
+}
+
+namespace KV
+{
+	template<class K,class V>
+	struct BSTreeNode
+	{
+		BSTreeNode<K, V>* _left;
+		BSTreeNode<K, V>* _right;
+		K _key;
+		V _value;
+
+		BSTreeNode(const K& key, const V& value)
+			:_left(nullptr)
+			,_right(nullptr)
+			,_key(key)
+			,_value(value)
+		{}
+	};
+
+
+	template<class K, class V>
+	class BSTree
+	{
+		typedef BSTreeNode<K, V> Node;
+	public:
+		bool Insert(const K& key, const V& value)
+		{
+			if (!_root)
+			{
+				_root = new Node(key, value);
+				return true;
+			}
+
+
+			Node* parent = nullptr;
+			Node* cur = _root;
+			while (cur)
+			{
+				if (cur->_key > key)
+				{
+					parent = cur;
+					cur = cur->_left;
+				}
+				else if (cur->_key < key)
+				{
+					parent = cur;
+					cur = cur->_right;
+				}
+				else
+				{
+					return false;
+				}
+			}
+
+			cur = new Node(key, value);
+			if (parent->_key < key)
+				parent->_right - cur;
+			else
+				parent->_left = cur;
+
+			return true;
+		}
+
+		Node* Find(const K& key)
+		{
+			Node* cur = _root;
+			while (cur)
+			{
+				if (cur->_key < key)
+				{
+					cur = cur->_right;
+				}
+				else if (cur->_key > key)
+				{
+					cur = cur->_left;
+				}
+				else
+				{
+					return cur;
+				}
+			}
+
+			return nullptr;
+		}
+
+		void InOrder()
+		{
+			_InOrder(_root);
+		}
+	private:
+		void _InOrder(Node* root)
+		{
+			_InOrder(root->_left);
+			cout << root->_key << ":" << root->_value << endl;
+			_InOrder(root->_right);
+		}
+
+	private:
+		Node* _root;
+	};
 }
--- BSTree/BSTree/test.cpp
+++ BSTree/BSTree/test.cpp
@@ -1,8 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include"BSTree.h"
 
-
-int main()
+void test1()
 {
 	K::BSTree<int> bst;
 
@@ -24,5 +23,51 @@ int main()
 	bst.Erase(4);
 	bst.Erase(6);
 	bst.InOrder();
+
+	bst.Insert(6);
+	bst.Insert(10);
+	bst.Insert(18);
+	bst.Insert(3);
+	bst.Insert(4);
+	bst.InOrder();
+
+	cout << "-------------" << endl;
+
+	K::BSTree<int> bst2(bst);
+
+	bst2.InOrder();
+
+	K::BSTree<int> bst3 = bst2;
+	bst3.Erase(18);
+	bst3.InOrder();
+	bst2.InOrder();
+}
+
+void test2()
+{
+	KV::BSTree<string, string>dict;
+	dict.Insert("Network", "网络");
+	dict.Insert("Operating System", "操作系统");
+	dict.Insert("Data structure", "数据结构");
+	dict.Insert("disk", "磁盘");
+	
+	string str;
+	while (cin >> str)
+	{
+		KV::BSTreeNode<string, string>* ret = dict.Find(str);
+		if (ret)
+		{
+			cout << ret->_value << endl;
+		}
+		else
+		{
+			cout << "查无此词" << endl;
+		}
+	}
+}
+int main()
+{
+	//test1();
+	test2();
 	return 0;
 }
